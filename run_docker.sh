8433bbb5d58b5f13dd8faeb3ee1889a6fe1cdf18
rawanmoustafa <62701975+rawanmoustafa@users.noreply.github.com>
2020-05-11 21:37:03 +0200

    Add files via upload

--- run_docker.sh
+++ run_docker.sh
@@ -3,13 +3,13 @@
 ## Complete the following steps to get Docker running locally
 
 # Step 1:
-# pull image
-sudo docker pull rawanmoustafa/capstone-repo:capstone
+# Build image
+docker build --tag=flasksklearn .
 
 # Step 2: 
 # List docker images
-sudo docker image ls
+docker image ls
 
 # Step 3: 
-# Run nginx web page
-sudo docker run -p 8000:80 capstone
+# Run flask app
+docker run -p 8000:80 flasksklearn
